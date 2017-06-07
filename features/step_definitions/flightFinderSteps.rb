186d255f6dadfdcad4911d43b5661ee0e09f4783
mauriValdi <mauricioo.valdi@hotmail.com>
2017-06-07 00:26:53 -0400

    deleted unnecessary line

--- features/step_definitions/flightFinderSteps.rb
+++ features/step_definitions/flightFinderSteps.rb
@@ -163,7 +163,6 @@ end
 Then(/^the values inserted for the are shown$/) do
   expect(page).to have_content(@passFirstname)
   expect(page).to have_content(@passLastname)
-  expect(page).to have_content(@creditNumber)
   expect(page).to have_content(@creditFirstName)
   expect(page).to have_content(@creditMidName)
   expect(page).to have_content(@creditLastName)
